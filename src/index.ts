665f69e0d040440e76aee26e02e774780b0cdd39
Lucas Della Bella <dellabella.lucas@gmail.com>
2021-04-21 21:15:35 -0700

    Add index file

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import schema from './graphql/schemasMap'
+
+const PORT = 4000
+
+const app = express()
+const server = new ApolloServer({
+    schema,
+})
+server.applyMiddleware({ app, path: '/graphql' })
+app.listen(PORT, () => {
+    console.log(`\nGraphQL is now running on http://localhost:${PORT}/graphql`
+})
+
+
