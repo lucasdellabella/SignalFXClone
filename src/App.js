a83ed5d0ae199cac4d6bf07cc22bb96464f64b3c
Lucas Della Bella <dellabella.lucas@gmail.com>
2021-04-27 17:00:36 -0700

    Set up most of react dummy app. Still needs id state + graphql client

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [singleShotDataInput, setSingleShotDataInput] = useState({count: 1})
+  const [multiShotDataInput, setMultiShotDataInput] = useState({count: 1, interval: 10000})
+
+  const sendDataPoints = ({ count }) => {
+    // do graphql mutation
+    console.log(count)
+  }
+
+  const initiateMultishotDataSequence = ({ count, interval }) => {
+    const intervalId = setInterval(() => {
+      sendDataPoints({count})
+    }, interval);
+    // TODO add rows for activate multishot sequences, make them cancellable
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <h1
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Send Data To SignalFXClone
+        </h1>
+        <div className="contents">
+          {/* Single shot burst */}
+          <div className="datapoint-input">
+            <div>
+              Send <input type="text" onChange={(e) => setSingleShotDataInput({count: e.target.value})} value={singleShotDataInput.count}/> 
+              datapoints.
+            </div>
+            <button onClick={() => sendDataPoints(singleShotDataInput)}>Send</button>
+          </div>
+          {/* repeated burst */}
+          <div className="datapoint-input">
+            <div>
+              Send <input type="text" onChange={(e) => setMultiShotDataInput({...multiShotDataInput, count: e.target.value})} value={multiShotDataInput.count}/> 
+              datapoints every <input type="text" onChange={(e) => setMultiShotDataInput({...multiShotDataInput, interval: e.target.value})} value={multiShotDataInput.interval}/>
+              seconds.
+            </div>
+            <button onClick={() => initiateMultishotDataSequence(multiShotDataInput)}>Send</button>
+          </div>
+          {/* Link to frontend app with graphs*/}
+        </div>
       </header>
     </div>
   );
